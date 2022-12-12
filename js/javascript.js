92f636515b1d774694ebf8e1dd6c75c8c8cc96b9
Cintiarandazo <115726854+Cintiarandazo@users.noreply.github.com>
2022-12-12 07:47:32 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+
+function inclusivo() {
+  document.body.style.backgroundColor= "black";
+  document.body.style.color= "white";
+
+}
+
+function autor() {
+alert (
+  "Pagina creada por Cintia Randazo mail:cintiarandazo@hotmail.com.")}
